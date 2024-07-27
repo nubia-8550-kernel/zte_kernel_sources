f2f0204b199253deb3ee68bf49e5b4879a111cea
anonymix007 <48598263+anonymix007@users.noreply.github.com>
2024-07-27 18:18:08 +0300

    Move kernel_platform/msm-kernel and kernel_platform/common to a separate repo

--- kernel_platform/msm-kernel/drivers/vendor/common/zlog/zlog_common/zlog_common.h
+++
@@ -1,6 +0,0 @@
-#ifndef _ZLOG_COMMON_H
-#define _ZLOG_COMMON_H
-
-#include <vendor/comdef/zlog_common_base.h>
-
-#endif
--- kernel_platform/msm-kernel/drivers/vendor/common/zlog/zlog_exception/zlog_exception.h
+++
@@ -1,6 +0,0 @@
-#ifndef _ZLOG_EXCEPTION_H
-#define _ZLOG_EXCEPTION_H
-
-ssize_t zlog_write_internal(const char *buf, size_t count);
-
-#endif

0404f51b8c858f9c2d53066620641e17809b74c4
anonymix007 <48598263+anonymix007@users.noreply.github.com>
2024-07-28 16:37:29 +0300

    Move vendor/qcom/opensource to separate repositories

--- vendor/qcom/opensource/display-drivers/msm/zte_disp/zte_disp_panel.h
+++
@@ -1,7 +0,0 @@
-#include "../dsi/dsi_panel.h"
-
-#define MAX_SLEEP_TIME 200
-
-void zte_disp_handle_lp_event(struct dsi_panel *panel, int power_mode);
-
-int zte_convert_backlevel_function(int level, u32 bl_max, u32 mode);
